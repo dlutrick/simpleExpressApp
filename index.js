348d92474d62cb2645106189238afaae429996ed
David <davidrlutrick@gmail.com>
2021-04-27 22:07:56 -0700

    Complete the basic functionality

--- index.js
+++ index.js
@@ -13,4 +13,25 @@ ourApp.get("/", function (req, res) {
     `);
 });
 
+ourApp.post("/answer", function (req, res) {
+  if (req.body.favoriteAnimal.length > 0) {
+    res.send(`
+    <p>${req.body.favoriteAnimal}'s are freaking awesome!</p>
+    <a href="/">Click to go back.</a>
+    `);
+  } else {
+    res.send(`
+      <p>You didn't enter anything! Try again!</p>
+      <a href="/">Click to go back.</a>
+      `);
+  }
+});
+
+ourApp.get("/answer", function (req, res) {
+  res.send(`
+  <p>Looking for something? There is nothing to see here.</p>
+  <a href="/">Click to go back.</a>
+  `);
+});
+
 ourApp.listen(3000);
